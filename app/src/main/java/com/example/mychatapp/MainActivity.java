d96e66abb0be5ed30fe2bb7c1b8117d2525b2ed9
Onur Kan <80545660+mehmetyildiz1812@users.noreply.github.com>
2021-07-01 22:29:18 +0300

    Final

--- app/src/main/java/com/example/mychatapp/MainActivity.java
+++ app/src/main/java/com/example/mychatapp/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.mychatapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
--- app/src/main/java/com/example/mychatapp/MessageActivity.java
+++ app/src/main/java/com/example/mychatapp/MessageActivity.java
@@ -1,11 +1,5 @@
 package com.example.mychatapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -13,7 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.mychatapp.Adapters.MessageAdapter;
@@ -26,7 +25,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.rengwuxian.materialedittext.MaterialEditText;
 
 import java.util.ArrayList;
 import java.util.HashMap;
